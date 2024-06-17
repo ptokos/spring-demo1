206e088293b2f96d287e3f7c3bd28dc09685d0d4
ptokos <jiseokchoij@gmail.com>
2024-06-17 11:23:17 +0900

    [feat]24.06.13 remind code-2

--- asac-spring-mvc/src/main/java/com/example/demo/error/CustomErrorController.java
+++ asac-spring-mvc/src/main/java/com/example/demo/error/CustomErrorController.java
@@ -1,2 +1,33 @@
-package com.example.demo.error;public class CustomErrorController {
+package com.example.demo.error;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/error")
+    public ModelAndView handleError(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView();
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Integer statusCode = Integer.valueOf(status.toString());
+
+        if (statusCode == HttpStatus.NOT_FOUND.value()) {
+            modelAndView.addObject("status", statusCode);
+            modelAndView.addObject("type", HttpStatus.NOT_FOUND.getReasonPhrase());
+        } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            modelAndView.addObject("status", statusCode);
+            modelAndView.addObject("type", HttpStatus.NOT_FOUND.getReasonPhrase());
+
+        }
+
+        modelAndView.setViewName("/custom/error");
+        return modelAndView;
+    }
 }
